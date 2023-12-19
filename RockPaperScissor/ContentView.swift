9458be2d42b11c9c0c36592c01d7ca204715b077
filecc <fil.gia@live.it>
2023-12-19 08:39:59 +0100

    complete

--- RockPaperScissor/ContentView.swift
+++ RockPaperScissor/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     @State private var winOrLose : Bool = Bool.random()
     private var agents = ["rock", "paper", "scissor"]
     @State private var iPhoneChoise = Int.random(in: 0 ... 2)
+    @State private var score : Int = 0
+    @State private var questions : Int = 1
+    @State private var isPresented : Bool = false
     
     var body: some View {
         VStack {
@@ -24,12 +27,13 @@ struct ContentView: View {
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 200, height: 200)
             }
+            
             Spacer()
             Text("Choose your fighter")
             HStack{
                 ForEach(agents, id: \.self){ agent in
                     Button(){
-                        
+                        aFighterWasTapped(agent: agent)
                     } label: {
                         Image("\(agent)")
                             .resizable()
@@ -39,8 +43,72 @@ struct ContentView: View {
                 }
             }
             
+        }.alert(isPresented: $isPresented){
+            Alert(
+                title: Text("You finish the game"),
+                message: Text("Your score is: \(score)"),
+                primaryButton: .default(Text("Restart"), action: restart), 
+                secondaryButton: .destructive(Text("Close"), action: restart)
+            )
         }
         .padding()
+    }
+    
+    
+    func restart () {
+        questions = 1
+        score = 0
+        iPhoneChoise = Int.random(in: 0...2)
+        winOrLose = Bool.random()
+        isPresented = false
+    }
+    
+    func aFighterWasTapped (agent: String){
+        if questions == 10 {
+            isPresented  = true
+        }
+        var correctAnswer : String
+        switch winOrLose {
+        case true:
+            switch iPhoneChoise {
+            case 0:
+                correctAnswer = "paper"
+                break;
+            case 1:
+                correctAnswer = "scissor"
+                break;
+            case 2:
+                correctAnswer = "rock"
+                break;
+            default:
+                correctAnswer = "even"
+            }
+        default:
+            switch iPhoneChoise {
+            case 0:
+                correctAnswer = "scissor"
+                break;
+            case 1:
+                correctAnswer = "rock"
+                break;
+            case 2:
+                correctAnswer = "paper"
+                break;
+            default:
+                correctAnswer = "even"
+            }
+        }
+        winOrLose = Bool.random()
+        iPhoneChoise = Int.random(in: 0 ... 2)
+        if(correctAnswer == agent){
+            score += 1
+        } else {
+            if(score > 0){
+                score -= 1
+            }
+           
+        }
+        questions += 1
     }
 }
 
